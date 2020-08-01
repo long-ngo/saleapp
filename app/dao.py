09c640b165300727802837616c84ffc70df68a55
longngo <ngothanhlongnoyssiw@gmail.com>
2020-08-01 17:13:43 +0700

    deleted .pyc

--- app/dao.py
+++ app/dao.py
@@ -4,9 +4,4 @@ import os
 
 def read_products():
     with open(os.path.join(app.root_path, "data/products.json"), encoding="utf-8") as f:
-        json.load(f)
-
-
-
-# if __name__ == "__main__":
-#     print(read_products())
+        return json.load(f)
--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import render_template
+
 from app import app, dao
+
 
 @app.route("/")
 def index():
